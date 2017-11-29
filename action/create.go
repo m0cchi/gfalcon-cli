388782ae132ffb96281db998a0d1efc4638ec358
m0cchi <boom.boom.planet@gmail.com>
2017-11-29 22:32:44 +0900

    add create action-link

--- action/create.go
+++ action/create.go
@@ -73,6 +73,48 @@ func CreateAction(c []string) {
 	fmt.Println(model.CreateAction(db, service.IID, actionID))
 }
 
+// args[0]: serviceID
+// args[1]: actionID
+// args[2]: teamID
+// args[3]: userID
+func CreateActionLink(c []string) {
+	if len(c) < 4 {
+		fmt.Println("gfalcon-cli create [target] [args...]")
+		return
+	}
+
+	serviceID := c[0]
+	actionID := c[1]
+	teamID := c[2]
+	userID := c[3]
+
+	service, err := model.GetService(db, serviceID)
+	if err != nil {
+		fmt.Printf("failed to get service: %v\n", err)
+		return
+	}
+
+	action, err := model.GetAction(db, service.IID, actionID)
+	if err != nil {
+		fmt.Printf("failed to get action: %v\n", err)
+		return
+	}
+
+	team, err := model.GetTeam(db, teamID)
+	if err != nil {
+		fmt.Printf("failed to get team: %v\n", err)
+		return
+	}
+
+	user, err := model.GetUser(db, team.IID, userID)
+	if err != nil {
+		fmt.Printf("failed to get user: %v\n", err)
+		return
+	}
+	fmt.Println(model.CreateActionLink(db, action, user))
+
+}
+
 func create(args []string) {
 	if len(args) < 1 {
 		fmt.Println("gfalcon-cli create [target] [args...]")
@@ -89,6 +131,8 @@ func create(args []string) {
 		CreateUser(args)
 	case "action":
 		CreateAction(args)
+	case "action-link":
+		CreateActionLink(args)
 	default:
 		fmt.Println("unknown command")
 	}
